c8c2b3d016a9dc8c6eb2fc628aa464149f500893
Batuhan Saygılı <batuhan.saygili@foreks.com>
2021-01-17 03:56:04 +0300

    Initial Commit

---
+++ simit/simit.h
@@ -0,0 +1,18 @@
+//
+//  simit.h
+//  simit
+//
+//  Created by Batuhan iOS Foreks on 17.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for simit.
+FOUNDATION_EXPORT double simitVersionNumber;
+
+//! Project version string for simit.
+FOUNDATION_EXPORT const unsigned char simitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simit/PublicHeader.h>
+
+
