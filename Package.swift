343cdc4e014b3506a4d5e919838b73da686a5aca
koust <batuhansaygili94@gmail.com>
2023-02-01 01:49:29 +0300

    fixes

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "simit",
-            dependencies: [.process("Resources")]),
+            dependencies: []),
         .testTarget(
             name: "simitTests",
             dependencies: ["simit"]),
