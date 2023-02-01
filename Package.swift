cedf4fd83d09b81d410cc4ca0458b7f0ae508526
Batuhan Saygılı <batuhan.saygili@foreks.com>
2023-02-01 18:11:24 +0300

    fixes

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "simit",
-            dependencies: []),
+            dependencies: [
+                "Ciao"
+            ]),
         .testTarget(
             name: "simitTests",
             dependencies: ["simit"]),
