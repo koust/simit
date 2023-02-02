c408a899a935b0f311587d5685cf17a4fc338d61
koust <batuhansaygili94@gmail.com>
2023-02-03 01:29:53 +0300

    fixes

--- Sources/simit/SimitRequestPackage.swift
+++ Sources/simit/SimitRequestPackage.swift
@@ -19,7 +19,7 @@ struct SimitRequestPackage:Codable {
     var deviceVersion:String = ""
     var request:SimitRequestURL!
     var response:SimitResponseURL!
-    var time:Date = Date().timeIntervalSince1970
+    var time:Double = Date().timeIntervalSince1970
     
 }
 
